c836e131d6d43f640c4e777ec802106245ec7dc6
fo0 <fo0systemsinfo@gmail.com>
2018-02-18 13:29:56 +0100

    changed cli api

--- Robot/src/main/java/com/fo0/robot/config/Config.java
+++ Robot/src/main/java/com/fo0/robot/config/Config.java
@@ -8,6 +8,6 @@ public class Config extends OptionsBase {
 	@Option(name = "nogui", abbrev = 'n', category = "GUI", help = "Graphical User Interface", defaultValue = "false")
 	public boolean nogui;
 
-	@Option(name = "cfg", abbrev = 'c', category = "config", help = "Read config by Path", defaultValue = "")
+	@Option(name = "config", abbrev = 'c', category = "Config", help = "Read config by Path", defaultValue = "")
 	public String configFile;
 }
